0ea219028dc2d4f7a58593aa493f425613429102
jmpizza <jespitiap@unal.edu.co>
2025-02-24 19:24:38 -0500

    test con procedimiento

--- src/app/api/test-db/route.js
+++ src/app/api/test-db/route.js
@@ -2,7 +2,7 @@ import pool from '@/lib/db';
 
 export async function GET() {
   try {
-    const [rows] = await pool.query('SHOW TABLES');
+    const [rows] = await pool.query("call ObtenerVentas(101123456);");
     return Response.json({ tables: rows });
   } catch (error) {
     return Response.json({ error: error.message }, { status: 500 });
