65337af27370b77136b0a12e887facab468904ff
jmpizza <jespitiap@unal.edu.co>
2025-02-26 01:36:58 -0500

    Conexion principal de la base de datos con la pagina, la cual permite acceso completo a la base de datos pero no necesariamente al usuario

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3307,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'elbuengusto',
 });
 
 export default pool;
