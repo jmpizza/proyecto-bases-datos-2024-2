3114e8a05fdd5ab38c84da47c24baff5398f23bb
Juan Espitia <138156930+jmpizza@users.noreply.github.com>
2025-02-27 15:21:10 +0000

    Adicion de estilos a la dashnavbar

--- src/app/components/dashnavbar.jsx
+++ src/app/components/dashnavbar.jsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Dashnavbar() {
   return (
-    <div>navbar</div>
+    <nav className="bg-yellow-300 text-black p-4 shadow-md">
+      <div className='w-full flex'>
+        <Image src="/logoElBuenGusto.png" alt="Logo" width={50} height={40} priority />
+        <ul className="flex ml-auto items-center space-x-4">
+          <li>
+            <Link href='/' className='bg-sky-300 hover:bg-sky-400 px-4 py-2 rounded-lg'>
+             Cerrar sesión
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
