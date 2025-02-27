7aeafe93f0cdaf8c90c05e0cf9691bc2597bd191
Juan Espitia <138156930+jmpizza@users.noreply.github.com>
2025-02-27 15:31:43 +0000

    Modificacion de la sidebar

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
     return (
-      <div className="bg-gray-200 w-1/6 min-w-[200px] h-screen p-4 shadow-md">
+      <div className="bg-gray-200 w-1/6 min-w-[200px] min-h-[calc(100vh-5rem)] p-4 shadow-md">
         Sidebar
       </div>
     );
