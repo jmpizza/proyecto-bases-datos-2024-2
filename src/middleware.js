910307d3101b32809edb1ecd43a41b3f49b0f95b
jmpizza <jespitiap@unal.edu.co>
2025-03-01 19:21:36 +0000

    Creacion y configuracion del middleware

---
+++ src/middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { verify } from "jsonwebtoken";
+
+export async function middleware(request) {
+    const jwt = request.cookies.get('loginToken');
+    if(jwt == undefined){
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+    const cookieHeader = request.headers.get("cookie") || "";
+    const response = await fetch("http://localhost:3000/api/auth/role", {
+      headers: { cookie: cookieHeader },
+    });
+    
+    const data = await response.json();
+    const role = data.role || "cliente";
+    if(role == 'GERENTE' || role == 'PANADERO' || role == 'PASTELERO' || role == 'CAJERO'){
+        return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL('/', request.url))
+}
+
+export const config = {
+    matcher: "/dashboard",
+}
